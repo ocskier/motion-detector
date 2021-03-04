1e44add021ca494f6290cb88af4c67370f8309f0
Jon Jackson <ocskier@gmail.com>
2021-03-04 00:50:39 -0500

    feat: capture initial starter frame

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
-import cv2
+import cv2, os
 
 video = cv2.VideoCapture(0)
+
+numFrames = 0
+compare = None
 
 while(True):
     check, frame = video.read()
     grey_image = cv2.cvtColor(frame,cv2.COLOR_BGR2GRAY)
+
+    if (numFrames == 0):
+        compare = grey_image
+        cv2.imwrite(os.path.join("./output","compare.jpg"),grey_image)
+
     cv2.imshow("front_cam",grey_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
