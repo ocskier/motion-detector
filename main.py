a079669fc0b567cd9ea9f09df7d8d6b4d7b02422
Jon Jackson <ocskier@gmail.com>
2021-03-07 00:47:04 -0500

    feat: add pipenv virt env, threshold and diff

--- main.py
+++ main.py
@@ -1,7 +1,8 @@
 import cv2, os
+from time import sleep
 
 video = cv2.VideoCapture(0)
-
+sleep(1)
 numFrames = 0
 compare = None
 
@@ -12,7 +13,12 @@ while(True):
         compare = grey_image
         cv2.imwrite(os.path.join("./output","compare.jpg"),grey_image)
     numFrames += 1
+    # print(numFrames)
+    frame_diff = cv2.absdiff(compare, grey_image)
+    ret,thres_image = cv2.threshold(frame_diff,127,255,cv2.THRESH_BINARY)
     cv2.imshow("front_cam",grey_image)
+    cv2.imshow('frame diff',frame_diff)
+    cv2.imshow("thres_diff",thres_image)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 print(numFrames)
